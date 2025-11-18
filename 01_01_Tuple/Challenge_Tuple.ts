e3b8c67fa073e94806313b8d5eb5126bcc71a81d
sandrolombardo <57590208+sandrolombardo@users.noreply.github.com>
2025-11-18 09:12:21 +0000

    tuple challenge gelöst

--- 01_01_Tuple/Challenge_Tuple.ts
+++ 01_01_Tuple/Challenge_Tuple.ts
@@ -1,12 +1,12 @@
 // Task 1:
-type XYTuple = any;
+type XYTuple = [number, number];
 
 const XY: XYTuple = [12, 39];
 const XY_Wrong: XYTuple = [12, 39, 20];
 
 // Task 2:
-type NameAgeHobbyTuple = any;
-const NameAgeHobby: NameAgeHobbyTuple = ["Anny", 25, "Surfing"];
+type NameAgeHobbyTuple = [string, number, string, ...string[]];
+const NameAgeHobby: NameAgeHobbyTuple = ["Anny", 25, "Surfing", "Gaming"];
 const NameAgeHobby_Wrong: NameAgeHobbyTuple = ["Anny", 25];
 
 export {};
