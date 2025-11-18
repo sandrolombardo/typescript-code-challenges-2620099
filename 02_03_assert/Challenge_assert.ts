2d534510c3b0e83f2cae639b70771e5847537e47
sandrolombardo <57590208+sandrolombardo@users.noreply.github.com>
2025-11-18 14:46:04 +0000

    challenge assert gelöst

--- 02_03_assert/Challenge_assert.ts
+++ 02_03_assert/Challenge_assert.ts
@@ -5,4 +5,12 @@ let myNumber = getValueFromServer();
 
 // assert(typeof myNumber === "number", "error -> value is not a number");
 
+function myAssert(value: any): asserts value is number {
+  if (typeof value !== 'number') {
+    throw Error('value is not a number');
+  }
+}
+
+myAssert(myNumber);
+
 type MyType = typeof myNumber;
